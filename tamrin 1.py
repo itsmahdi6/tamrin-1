1854398ad7c5807455a3bcb76abfd8ad662b197a
itsmahdi6 <mahdi.1383hajix@gmail.com>
2025-12-16 09:33:51 +0330

    Add files via upload

---
+++ tamrin 1.py
@@ -0,0 +1,5 @@
+Name =input("please enter your name: ")
+BirthYear = int (input("please enter your birthyear"))
+CurrentYear =1404
+age = CurrentYear - BirthYear
+print(f"nice to meet you {Name}! your age is {age}")
